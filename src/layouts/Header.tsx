d76e3693283677ae1340b7be839cc5357c1eeb8b
김섭찬 <rlatjqcks@gmail.com>
2024-06-23 07:00:23 +0900

    [feat] 회원가입 구현
    - navigation 사용 로그인 화면 이동
    
    - firebase SDK 연동
    
    - contextAPI로 auth 전역 상태 관리
    - AuthGuard 생성
    - useAuth 훅 생성
    - AuthContext 생성

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,10 @@
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <div
+      className="w-full h-16 flex justify-center items-center bg-gray-200 border-2 border-green-500"
+    >
+      <h1> Header </h1>
+    </div>
   );
 }
 
--- src/pages/commerce/HomePage.tsx
+++ src/pages/commerce/HomePage.tsx
@@ -1,10 +1,26 @@
+import { Button } from '@/components/ui/button.tsx';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Home Page</h1>
-
-
+      {/* 회원가입 화면으로 이동 */}
+      <Button
+        onClick={() => {
+          // 네비게이션으로 이동
+          navigate('/signup-selection');
+        }}
+      >회원가입</Button>
+      {/* 로그인 화면으로 이동 */}
+      <Button
+        onClick={() => {
+          // 네비게이션으로 이동
+          navigate('/signup-selection');
+        }}
+      >로그인</Button>
     </div>
   );
 }
